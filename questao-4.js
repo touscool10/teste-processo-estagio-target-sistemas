091d5d1d8277dd7d8821b551df5a602e87bc0cdb
Setonde T. C. Nougbodohoue <touscool10@gmail.com>
2023-04-26 02:21:08 -0300

    finished test - question 4  resolved

---
+++ questao-4.js
@@ -0,0 +1,7 @@
+// Quando eles se cruzarem na rodovia, qual estará mais próximo a cidade de Ribeirão Preto?
+
+
+
+// Quando eles se cruzarem na rodovia, eles estarão no mesmo lugar. Isso significa que
+// os dois estarão mais próximo a cidade de Ribeirão Preto. Cada um estará à mesma distancia
+// da cidade de Ribeirão Preto
